77810410f156eca08443921d82fa851cb18aac15
DREEEEI <163445990+DREEEEI@users.noreply.github.com>
2024-04-02 14:01:49 +0800

    Update Program.cs

--- Assignment#1/Program.cs
+++ Assignment#1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-// THIS PROGRAM IS CREATED BY:
+//   THIS PROGRAM IS CREATED BY:
 
 //   XXXXx   XXXXx   XXXXXX  XXX
 //   XX  XX  XX  XX  XX      XXX
 //   XX  XX  XXXXx   XXXXX   XXX
 //   XX  XX  XX  XX  XX      XXX
-//   XXXXX   XX  XX  XXXXXX  XXX
+//   XXXXx   XX  XX  XXXXXX  XXX
 
 
 namespace CodeByDrei
@@ -15,71 +15,85 @@ namespace CodeByDrei
     internal class Program
     {
         static void Main(string[] args)
-
         {
-            Console.Write("Enter Username:");
+            Console.Write("Enter Username: ");
             string Username = Console.ReadLine();
 
-            if (Username == "Admin") ;
-
-            Console.Write("Enter Password:");
+            Console.Write("Enter Password: ");
             string Password = Console.ReadLine();
 
-            if (Password == "Admin1234!") ;
+            if (Username == "Admin" && Password == "Admin1234!") 
+            {
+                Console.WriteLine("\n\nLogged in successfully.");
+                return;
+            }
 
             else
             {
                 Console.WriteLine("\n\nWrong credentials, please try again. (3 attempts left)");
-
-                Console.Write("\nEnter Username:");
+            }
+            {
+                Console.Write("\n\nEnter Username: ");
                 Username = Console.ReadLine();
 
-                if (Username == "Admin") ;
-
-                Console.Write("Enter Password:");
+                Console.Write("Enter Password: ");
                 Password = Console.ReadLine();
 
-                if (Password == "Admin1234!") ;
+                if (Username == "Admin" && Password == "Admin1234!")
 
                 {
-                    Console.Write("\n\nWrong credentials, please try again. (2 attempts left)");
+                    Console.WriteLine("\n\nLogged in successfully.");
+                }
 
-                    Console.Write("\n\nEnter Username:");
+                else
+                { 
+                    Console.WriteLine("\n\nWrong credentials, please try again. (2 attempts left)");
+                }
+                {
+                    Console.Write("\n\nEnter Username: ");
                     Username = Console.ReadLine();
 
-                    if (Username == "Admin") ;
-
-                    Console.Write("Enter Password:");
+                    Console.Write("Enter Password: ");
                     Password = Console.ReadLine();
 
-                    if (Password == "Admin1234!") ;
-
+                    if (Username == "Admin" && Password == "Admin1234!")
 
                     {
-                        Console.Write("\n\nWrong credentials, please try again. (1 attempt left)");
+                        Console.WriteLine("\n\nLogged in successfully.");
+                        return;
+                    }
 
-                        Console.Write("\n\nEnter Username:");
+                    else
+                    {
+                        Console.WriteLine("\n\nWrong credentials, please try again. (1 attempt left)");
+                    }
+                    {
+                        Console.Write("\n\nEnter Username: ");
                         Username = Console.ReadLine();
 
-                        if (Username == "Admin") ;
-
-                        Console.Write("Enter Password:");
+                        Console.Write("Enter Password: ");
                         Password = Console.ReadLine();
 
-                        if (Password == "Admin1234!") ;
+                        if (Username == "Admin" && Password == "Admin1234!")
+
+                        {
+                            Console.WriteLine("\n\nLogged in successfully.");
+                            return;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("\n\nYour account has been temporarily locked, please try again later.");
+                        }
 
                     }
 
-
                 }
 
-
             }
-
 
         }
 
     }
 
 }
-        
